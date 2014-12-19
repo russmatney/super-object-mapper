948a77c3bebab13c1059248e55a9eeb86accf3a6
Russell Matney <russell.matney@gmail.com>
2014-12-19 15:17:11 -0500

    mocha, chai, structure, first test

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/super-object-mapper');
---
+++ test/super-object-mapper.spec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var SuperOM = require('../');
+
+describe('Super Object Mapper', function() {
+  it('exists', function() {
+    expect(SuperOM).to.exist();
+  });
+});
