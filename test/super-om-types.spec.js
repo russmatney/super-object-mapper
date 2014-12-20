97f1ba1bf43bb9a449b35ca1997bd3da7df199e7
Russell Matney <russell.matney@gmail.com>
2014-12-20 00:55:02 -0500

    handling for null, conversions from int to string

--- test/super-om-types.spec.js
+++ test/super-om-types.spec.js
@@ -10,6 +10,50 @@ describe('Super Object Mapper type enforcement', function() {
   describe('.string()', function(){
     var superOM = new SuperOM();
 
+    it('should convert keys properly', function() {
+      superOM.addMapper({
+        "domain": {
+          "name": superOMType.string("doodle")
+        }
+      }, "users");
+      var object = {
+        name: "Johnny"
+      };
+      var mappedObject = superOM.mapObject("domain", "users", object);
+
+      expect(mappedObject.doodle).to.be.a("string").and.equal(object.name)
+        .and.exist();
+    });
+
+    it('should return nothing if value is null', function() {
+      superOM.addMapper({
+        "domain": {
+          "name": superOMType.string("name")
+        }
+      }, "users");
+      var object = {
+        name: null
+      };
+      var mappedObject = superOM.mapObject("domain", "users", object);
+
+      console.log(mappedObject);
+
+      expect(mappedObject.name).not.to.exist();
+    });
+
+    it('should return nothing if value is not passed', function() {
+      superOM.addMapper({
+        "domain": {
+          "name": superOMType.string("name")
+        }
+      }, "users");
+      var object = { };
+      var mappedObject = superOM.mapObject("domain", "users", object);
+
+      expect(mappedObject.name).not.to.exist();
+    });
+
+
     it('should convert strings to strings', function() {
       superOM.addMapper({
         "domain": {
@@ -21,7 +65,24 @@ describe('Super Object Mapper type enforcement', function() {
       };
       var mappedObject = superOM.mapObject("domain", "users", object);
 
-      expect(typeof mappedObject.name).to.equal("string");
+      expect(mappedObject.name).to.be.a("string").and.equal(object.name)
+        .and.exist();
+    });
+
+    it('should convert numbers to strings', function() {
+      superOM.addMapper({
+        "domain": {
+          "count": superOMType.string("count")
+        }
+      }, "users");
+      var object = {
+        count: 8
+      };
+      var mappedObject = superOM.mapObject("domain", "users", object);
+
+      expect(mappedObject.count).to.be.a("string").and.equal('8')
+        .and.exist();
+
     });
 
   });
